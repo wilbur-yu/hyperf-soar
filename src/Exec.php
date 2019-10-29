0a8c7021f185a41147b1f399bb7e22844b573e0c
wilbur-oo <ziyanziyu77@gmail.com>
2019-10-29 19:02:31 +0800

    up exec by Swoole\Coroutine\System::exec

--- src/Exec.php
+++ src/Exec.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace Wilbur\HyperfSoar;
 
 use Guanguans\SoarPHP\Exceptions\InvalidArgumentException;
+use Guanguans\SoarPHP\Exceptions\RuntimeException;
 use Swoole\Coroutine\System;
 
 trait Exec
@@ -13,6 +14,7 @@ trait Exec
 	 *
 	 * @return string|null
 	 * @throws InvalidArgumentException
+	 * @throws RuntimeException
 	 */
 	public function exec($command) : ?string
 	{
@@ -24,6 +26,12 @@ trait Exec
 			throw new InvalidArgumentException(sprintf("Command error: '%s'", $command));
 		}
 
-		return System::exec($command);
+		$result = System::exec($command);
+
+		if (0 !== $result['code']) {
+			throw new RuntimeException(sprintf("Command error: '%s'", $result['output']));
+		}
+
+		return $result['output'];
 	}
 }
