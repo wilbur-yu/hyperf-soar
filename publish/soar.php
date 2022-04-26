0c7641617ca318c7c00f6fb3fa52c922aded9f52
wenbo <wenbo@wenber.club>
2022-04-26 22:41:57 +0800

    更新配置信息

--- publish/soar.php
+++ publish/soar.php
@@ -14,21 +14,37 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    'enabled' => env('SOAR_ENABLED', env('APP_ENV') === 'local'),
+    'enabled' => env('SOAR_ENABLED', true),
     'cut_classes' => [
         'Hyperf\HttpServer\Response::json',
     ],
-    '-soar-path' => env('SOAR_PATH', ''), // soar 二进制文件的绝对路径
+    // soar 二进制文件的绝对路径
+    '-soar-path' => env('SOAR_PATH'),
+    // 测试环境配置
     '-test-dsn' => [
         'host' => env('SOAR_TEST_DSN_HOST', '127.0.0.1'),
         'port' => env('SOAR_TEST_DSN_PORT', '3306'),
         'dbname' => env('SOAR_TEST_DSN_DBNAME', 'database'),
         'username' => env('SOAR_TEST_DSN_USER', 'root'),
         'password' => env('SOAR_TEST_DSN_PASSWORD', ''),
-        'disable' => env('SOAR_TEST_DSN_DISABLE', false),
+        'disable' => env('SOAR_TEST_DSN_DISABLE', true),
     ],
-    '-sampling' => env('SOAR_SAMPLING', true),                       // 是否开启数据采样开关
-    '-allow-drop-index' => env('SOAR_ALLOW_DROP_INDEX', true),       // 允许输出删除重复索引的建议
-    '-drop-test-temporary' => env('SOAR_DROP_TEST_TEMPORARY', true), // 是否清理测试环境产生的临时库表
+    // 是否开启数据采样开关
+    '-sampling' => env('SOAR_SAMPLING', true),
+    // 允许输出删除重复索引的建议
+    '-allow-drop-index' => env('SOAR_ALLOW_DROP_INDEX', true),
+    // 是否清理测试环境产生的临时库表
+    '-drop-test-temporary' => env('SOAR_DROP_TEST_TEMPORARY', true),
+    // 日志输出文件
     '-log-output' => BASE_PATH.'/runtime/logs/soar.log',
+    /*
+     * 启发式算法相关配置
+     */
+    '-max-join-table-count' => 5,
+    '-max-group-by-cols-count' => 5,
+    '-max-distinct-count' => 5,
+    '-max-index-cols-count' => 5,
+    '-max-total-rows' => 9999999,
+    '-spaghetti-query-length' => 2048,
+    // '-allow-drop-index' => false,
 ];
