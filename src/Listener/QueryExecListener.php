cecb207b36837fc9ff9aaac1892649b58215ac4d
pandaLIU <563883861@qq.com>
2023-01-12 11:08:12 +0800

    fixed QueryExecListener

--- src/Listener/QueryExecListener.php
+++ src/Listener/QueryExecListener.php
@@ -2,17 +2,9 @@
 
 declare(strict_types=1);
 /**
- * This file is part of project hyperf-soar.
- *
- * @author   wenber.yu@creative-life.club
- * @link     https://github.com/wilbur-yu
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
  */
-
 namespace Wilbur\HyperfSoar\Listener;
 
 use Hyperf\Contract\ConfigInterface;
@@ -23,16 +15,16 @@ use Hyperf\Utils\Arr;
 use Hyperf\Utils\Context;
 use Hyperf\Utils\Str;
 
-
 #[Listener]
 class QueryExecListener implements ListenerInterface
 {
-    protected bool $soarIsEnabled;
     public const SQL_RECORD_KEY = 'wilbur-yu-hyperf-soar-sql-listener';
+
+    protected bool $soarIsEnabled;
 
     public function __construct(ConfigInterface $config)
     {
-        $this->soarIsEnabled = $config->get('soar.enabled');
+        $this->soarIsEnabled = $config->get('soar.enabled', false);
     }
 
     public function listen(): array
@@ -46,12 +38,12 @@ class QueryExecListener implements ListenerInterface
     {
         if ($event instanceof QueryExecuted && $this->soarIsEnabled) {
             $sql = str_replace('`', '', $event->sql);
-            if (!Arr::isAssoc($event->bindings)) {
+            if (! Arr::isAssoc($event->bindings)) {
                 foreach ($event->bindings as $value) {
-                    $sql = Str::replaceFirst('?', "'$value'", $sql);
+                    $sql = Str::replaceFirst('?', "'{$value}'", $sql);
                 }
             }
-            $eventSqlList = (array)Context::get(self::SQL_RECORD_KEY);
+            $eventSqlList = (array) Context::get(self::SQL_RECORD_KEY);
             $eventSqlList[] = $sql;
             Context::set(self::SQL_RECORD_KEY, $eventSqlList);
         }
